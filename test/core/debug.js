fc64b3488b2a5b1b404730fbf8ba7f7e31938379
fantasyni <fantasyni@163.com>
2015-04-23 10:40:08 +0800

    review ddb and mysql cluster directPlan

--- test/core/debug.js
+++ test/core/debug.js
@@ -14,18 +14,47 @@ var sqlDirPath = path.dirname(sqlPath);
 bearcatDao.loadSQL([sqlDirPath]);
 
 process.env.LOGGER_LINE = true;
+process.env.BEARCAT_DEBUG = true;
 bearcat.start(function() {
 	var domainDaoSupport = bearcat.getBean('domainDaoSupport');
 	domainDaoSupport.initConfig("person");
 
 	var id = 1;
-	domainDaoSupport.getList("$blogResultSql", id, "blogResult", function(err, results) {
-		err = err || true;
-		console.log(err.stack);
+	// domainDaoSupport.getList("$blogResultSql", id, "blogResult", function(err, results) {
+	// 	err = err || true;
+	// 	console.log(err.stack);
 
-		// console.log(results);
-		var blogResult = results;
-		blogResult.run();
-		// console.log(blogResult);
-	})
+	// 	// console.log(results);
+	// 	var blogResult = results;
+	// 	blogResult.run();
+	// 	// console.log(blogResult);
+	// })
+	var domainDaoSupport = bearcat.getBean('domainDaoSupport');
+	domainDaoSupport.initConfig(personDomain);
+	var person1 = domainFactory.getDomain(personDomain);
+	person1.setName('yyy');
+	person1.setNum(100);
+	person1.setCreateAt(Date.now());
+
+	var list = [];
+	list.push(person1);
+
+	var person2 = domainFactory.getDomain(personDomain);
+	person2.setName('bbb');
+	person2.setNum(200);
+	person2.setCreateAt(Date.now());
+
+	list.push(person2);
+
+	var person3 = domainFactory.getDomain(personDomain);
+	person3.setId(Date.now() / 1000);
+	person3.setName('ccc');
+	person3.setNum(300);
+	person3.setCreateAt(Date.now());
+
+	list.push(person3);
+
+	domainDaoSupport.batchAdd(list, function(err, results) {
+		console.log(err);
+	});
 });
